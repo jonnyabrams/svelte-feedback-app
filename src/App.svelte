d0fb8e11060f19262df458361094a969600df7a6
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-15 16:23:31 +0100

    feat: set up FeedbackList, FeedbackItem & Card components + FeedbackList styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,27 @@
 <script>
-	
+	import FeedbackList from './components/FeedbackList.svelte'
+
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'oi oi'
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'oi oiiiiii'
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: 'oi oiiiiiiiiiiiiii'
+		},
+	]
 </script>
 
 <main>
-	<h1>Hello world</h1>
+	<FeedbackList {feedback} />
 	 
 </main>
 
