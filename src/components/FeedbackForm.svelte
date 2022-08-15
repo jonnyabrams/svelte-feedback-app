9b0f73e69be81dd1a5d57944d323321a883e5d9b
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-15 17:02:29 +0100

    feat: create form with button

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,57 @@
+<script>
+  import Card from "./Card.svelte";
+  import Button from "./Button.svelte";
+
+  let text = ""
+  let btnDisabled = true
+</script>
+
+<Card>
+  <header>
+    <h2>How would you rate our service?</h2>
+  </header>
+</Card>
+<form>
+  <div class="input-group">
+    <input type="text" bind:value = {text} placeholder="Have your say...">
+    <Button disabled={btnDisabled} type="submit">Send</Button>
+  </div>
+</form>
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  .message{
+    padding-top: 10px;
+    text-align: center;
+    color: rebeccapurple;
+  }
+</style>
